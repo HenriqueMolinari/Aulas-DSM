c44e7d3e3fd6d8a0ef5782b4cb78f0d9264f40f3
Henrique Molinari <henrique.molinari@sou.unifeob.edu.br>
2026-03-12 22:20:54 -0300

    Adicionando um Container com Shadow

--- projeto01/lib/ui/splash_page.dart
+++ projeto01/lib/ui/splash_page.dart
@@ -7,7 +7,7 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-        padding: EdgeInsets.all(100),
+        padding: EdgeInsets.all(16),
         color: const Color.fromARGB(255, 255, 255, 255),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -16,24 +16,64 @@ class SplashPage extends StatelessWidget {
             Row(
               children: [
                 Container(
-                  width: 150,
-                  height: 150,
+                  width: 52,
+                  height: 52,
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
                     color: Colors.black,
-                    borderRadius: BorderRadius.circular(14),
+                    borderRadius: BorderRadius.circular(16),
                   ),
                   child: Text(
-                    'IA',
+                    'A',
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 100,
+                      fontSize: 22,
                       fontWeight: FontWeight.w800,
                       decoration: TextDecoration.none,
                     ),
                   ),
                 ),
+                const SizedBox(width: 12),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Aula Flutter',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w800,
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                    SizedBox(width: 12),
+                    Text(
+                      'Splash Só Layout',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.black54,
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                  ],
+                ),
               ],
+            ),
+            SizedBox(height: 28),
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(18),
+                border: Border.all(color: Color(0x1100000)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black87,
+                    blurRadius: 18,
+                    offset: Offset(0, 10),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
